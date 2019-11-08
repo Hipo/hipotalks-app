fcc8a2704dca33ea90bebcc12b0e2203fd40cdcb
hacernur <hacernuryanaral@gmail.com>
2019-11-08 17:09:37 +0300

    static files added to nginx

--- hipotalks/settings/development.py
+++ hipotalks/settings/development.py
@@ -1,4 +1,6 @@
 import redis
+from os.path import dirname, abspath
+
 from .base import *
 from .secrets_development import *
 
@@ -24,3 +26,13 @@ REDIS_CELERY_NUMBER = 1
 REDIS_CONNECTION = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=3, health_check_interval=20)
 
 BASE_URL = 'http://localhost:8000'
+
+
+MEDIA_ROOT = os.path.join(dirname(dirname(dirname(abspath(__file__)))), 'media/files/')
+STATIC_ROOT = os.path.join(dirname(dirname(dirname(abspath(__file__)))), 'static/')
+
+STATICFILES_DIRS = (
+    ('', abspath(os.path.join(dirname(dirname(dirname(abspath(__file__)))), 'frontend/'))),
+)
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
